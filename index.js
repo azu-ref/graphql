756d2130ed4fa4d594713f1b949b9e401d9c87b8
azu-ref <fjah17@gmail.com>
2019-06-25 19:09:57 -0400

    creando servidor y custom types Courses

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql');
+const express = require('express');
+const gqlMiddleware = require('express-graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const resolvers = require('./lib/resolvers')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
 
 //Definiendo esquema
-const schema = buildSchema(`
-    type Query {
-        hello: String,
-        saludo: String
-    }
-`)
+const schema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+        )
 
-const resolvers = {
-    hello: () => 'Hola Mundo',
-    saludo: () => 'Hola a todos'
-}
 
-//Ejecutarel query hello
-graphql(schema, '{ saludo }', resolvers).then( data => {
-    console.log(data)
-})
+app.use('/api', gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+}));
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}/api`)
+});
